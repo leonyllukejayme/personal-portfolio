36c1160b70bd5daabd1e9a7dd7db78107ce219bc
leonyllukejayme <leonyljayme@gmail.com>
2024-01-22 21:00:35 +0800

    Update

--- src/components/Project_card.jsx
+++ src/components/Project_card.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import project1 from '../images/tindog.jpg'
 
 const Project_card = ({
 	id,
@@ -11,26 +10,11 @@ const Project_card = ({
 }) => {
 	return (
 		<>
-			{/* <div className="card" style={{ width: '18rem' }}>
+			<div className="card" style={{ width: '25rem' }}>
 				<img src={projImg} className="card-img-top" alt={title} />
 				<div className="card-body">
 					<h5 className="card-title">{title}</h5>
 					<p className="card-text">{description}</p>
-					<div className="d-flex gap-3">
-                    <a href={sourceCodeURL} className="btn btn-primary">
-						Source Code
-					</a>
-					<a href={demoURL} className="btn btn-secondary">
-						Demo
-					</a>
-                    </div>
-				</div>
-			</div> */}
-			<div className="card" style={{ width: '25rem' }}>
-				<img src={project1} className="card-img-top" alt={title} />
-				<div className="card-body">
-					<h5 className="card-title">TinDog Website</h5>
-					<p className="card-text">A dynamic web application crafted using HTML, CSS, and Bootstrap. It seamlessly combines elegant design with responsive functionality to create an engaging platform for dog lovers to connect and explore canine companionship</p>
 					<div className="d-flex gap-3">
                     <a href={sourceCodeURL} className="btn btn-primary">
 						Source Code
--- src/components/Projects_sect.jsx
+++ src/components/Projects_sect.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Project_card from './Project_card';
+import project1 from '../images/tindog.jpg'
 
 const Projects_sect = () => {
 	return (
@@ -8,13 +9,14 @@ const Projects_sect = () => {
 				<div className="container justify-content-center text-center my-5">
 					<h1>Projects</h1>
 				</div>
+                <hr class="w-50 mx-auto mb-5 text-secondary" />
 				<div className="row justify-content-center gap-4">
-					<Project_card />
-					<Project_card />
-					<Project_card />
-					<Project_card />
-					<Project_card />
-					<Project_card />
+					<Project_card id={1} projImg={project1} title={"TinDog Website"} description={"A dynamic web application crafted using HTML, CSS, and Bootstrap. It seamlessly combines elegant design with responsive functionality to create an engaging platform for dog lovers to connect and explore canine companionship"} />
+					<Project_card id={1} projImg={project1} title={"TinDog Website"} description={"A dynamic web application crafted using HTML, CSS, and Bootstrap. It seamlessly combines elegant design with responsive functionality to create an engaging platform for dog lovers to connect and explore canine companionship"} />
+					<Project_card id={1} projImg={project1} title={"TinDog Website"} description={"A dynamic web application crafted using HTML, CSS, and Bootstrap. It seamlessly combines elegant design with responsive functionality to create an engaging platform for dog lovers to connect and explore canine companionship"} />
+					<Project_card id={1} projImg={project1} title={"TinDog Website"} description={"A dynamic web application crafted using HTML, CSS, and Bootstrap. It seamlessly combines elegant design with responsive functionality to create an engaging platform for dog lovers to connect and explore canine companionship"} />
+					<Project_card id={1} projImg={project1} title={"TinDog Website"} description={"A dynamic web application crafted using HTML, CSS, and Bootstrap. It seamlessly combines elegant design with responsive functionality to create an engaging platform for dog lovers to connect and explore canine companionship"} />
+					<Project_card id={1} projImg={project1} title={"TinDog Website"} description={"A dynamic web application crafted using HTML, CSS, and Bootstrap. It seamlessly combines elegant design with responsive functionality to create an engaging platform for dog lovers to connect and explore canine companionship"} />
 				</div>
 			</div>
 		</>
