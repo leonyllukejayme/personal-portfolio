9b9b081d2907b9fe55e502cf3bdfbfb913ae3c40
leonyllukejayme <leonyljayme@gmail.com>
2024-02-01 22:03:30 +0800

    from "Portfolio" to a logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png'
 
 function Navbar() {
     const [color, setColor] = useState(false);
@@ -17,7 +18,7 @@ function Navbar() {
   return (
         <nav className={color ? 'navbar navbar-expand-lg fixed-top header-bg' : 'navbar navbar-expand-lg fixed-top'}>
             <div className='container-fluid mx-5 my-2'>
-                <Link to="/" className='h1 navbar-brand' style={{fontSize: '2rem'}}>Portfolio</Link>
+                <Link to="/" className=' navbar-brand'><img src={logo} alt="</>" width={46} /></Link>
                 <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle Navigation'>
                     <span className='navbar-toggler-icon'></span>
                 </button>
