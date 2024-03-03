32c894e125428c52bcaa9577c09fedc06d4276a8
leonyllukejayme <leonylljayme@gmail.com>
2024-03-03 21:52:04 +0800

    update project_card

--- src/components/Project_card.jsx
+++ src/components/Project_card.jsx
@@ -9,36 +9,45 @@ const Project_card = ({
 	sourceCodeURL,
 	demoURL,
 }) => {
-	const[isReadMore,setIsReadMore] = useState(true);
-	const toggleReadMore = () => {setIsReadMore(!isReadMore)}
+	const [isReadMore, setIsReadMore] = useState(true);
+	const toggleReadMore = () => {
+		setIsReadMore(!isReadMore);
+	};
 
-	const shortDesc = description.substr(0,150) + '...';
+	const shortDesc = description.substr(0, 150) + '...';
 	return (
 		<>
 			<div className="card" style={{ width: '25rem' }}>
 				<img src={projImg} className="card-img-top" alt={title} />
 				<div className="card-body">
 					<h5 className="card-title">{title}</h5>
-					<div className="d-flex gap-2">
-					{pLang.map(p => <p class="skl-btn">{p}</p>)}
+					<div className="d-flex flex-wrap gap-2">
+						{pLang.map((p) => (
+							<p class="skl-btn">{p}</p>
+						))}
 					</div>
-					<p className="card-text">{isReadMore ? shortDesc : description}{
-						description.length > 150 && <span className='read-more' onClick={toggleReadMore}>
-							{isReadMore ? 'Read More' : ' Show Less'}
-						</span>
-					}</p>
+					<p className="card-text">
+						{isReadMore ? shortDesc : description}
+						{description.length > 150 && (
+							<span className="read-more" onClick={toggleReadMore}>
+								{isReadMore ? 'Read More' : ' Show Less'}
+							</span>
+						)}
+					</p>
 					<div className="d-flex gap-3">
-                    <a href={sourceCodeURL} target='_blank' className="btn btn-primary">
-						Source Code
-					</a>
-					<a href={demoURL} target='_blank' className="btn btn-secondary">
-						Demo
-					</a>
-                    </div>
+						<a href={sourceCodeURL} target="_blank" className="btn btn-primary">
+							Source Code
+						</a>
+						{demoURL && (
+							<a href={demoURL} target="_blank" className="btn btn-secondary">
+								Demo
+							</a>
+						)}
+					</div>
 				</div>
 			</div>
 		</>
 	);
-};	
+};
 
 export default Project_card;
